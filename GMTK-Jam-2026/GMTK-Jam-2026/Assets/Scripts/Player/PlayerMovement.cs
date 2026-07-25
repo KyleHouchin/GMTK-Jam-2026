1ea5a9c046b9625dd5076590ea1c50d3b8e52fb6
OMEGA-H-S <captainhs101@gmail.com>
2026-07-25 17:04:27 -0400

    Wall jumping improved!

--- GMTK-Jam-2026/GMTK-Jam-2026/Assets/Scripts/Player/PlayerMovement.cs
+++ GMTK-Jam-2026/GMTK-Jam-2026/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -43,6 +44,20 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Runtime Speed Modifier")]
     [SerializeField] private float externalSpeedMultiplier = 1f;
+
+    [Header("Wall Mechanics")]
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallSlidingSpeed = 2f;
+    [SerializeField] private float timeBeforeSlipping = 0.5f;
+    private float timeBeforeSlippingCounter;
+    private bool isWallSliding;
+    private bool isWallJumping;
+    [SerializeField] private Vector2 wallJumpMagnitude = new Vector2(8, 16);
+    private float wallJumpDirection;
+    [SerializeField] private float wallJumpTime = 0.2f;
+    private float wallSlidingBufferCounter;
+    [SerializeField] private float wallSlidingBuffer = 0.12f;
 
     public float ExternalSpeedMultiplier =>
         externalSpeedMultiplier;
@@ -127,6 +142,8 @@ public class PlayerMovement : MonoBehaviour
         UpdateJumpBuffer();
         UpdateDashCooldownCounter();
         UpdateAnimation();
+        IsWalled();
+        TryToWallJump();
     }
 
     private void FixedUpdate()
@@ -134,7 +151,44 @@ public class PlayerMovement : MonoBehaviour
         ApplyHorizontalMovement();
         TryToJump();
         ApplyVariableJumpHeight();
+        WallSlide();
     }
+
+    private void TryToWallJump()
+    {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            wallSlidingBufferCounter = 0;
+            Debug.Log("This is the case");
+        }
+        else
+        {
+            wallSlidingBufferCounter += Time.deltaTime;
+        }
+        Debug.Log(isWallSliding + " " + (wallSlidingBufferCounter < wallSlidingBuffer));
+        //You can wall jump if you are currently sliding down a wall (isWallSliding = true)
+        if (isWallSliding && wallSlidingBufferCounter < wallSlidingBuffer)
+        {
+            Debug.Log("This is the case too");
+            wallSlidingBufferCounter = 0;
+            //The jump itself
+            //Flip the character if they are facing the wrong way
+            wallJumpDirection = facingRight ? -1 : 1;
+            FlipFacingDirection();
+            isWallJumping = true;
+            isWallSliding = false;
+            playerRigidbody.linearVelocity = new Vector2(wallJumpDirection * wallJumpMagnitude.x, wallJumpMagnitude.y);
+            StartCoroutine(WallJumpDuration());
+        }
+
+    }
+
+    private IEnumerator WallJumpDuration()
+    {
+        yield return new WaitForSeconds(wallJumpTime);
+        isWallJumping = false;
+
+    } 
 
     private void ReadMovementInput()
     {
@@ -195,6 +249,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimation()
     {
+        if(isWallJumping)
+        {
+            Debug.Log("Wall jump happening, move on");
+            return;
+        }
         if (anim != null)
         {
             anim.SetFloat(
@@ -250,6 +309,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void ApplyHorizontalMovement()
     {
+        if (isWallJumping)
+        {
+            return;
+        }
         float newHorizontalSpeed;
 
         if (!isDashing)
@@ -396,7 +459,8 @@ public class PlayerMovement : MonoBehaviour
     public void SetDash()
     {
         if (isDashing ||
-            dashCooldownCounter < dashCooldown)
+            dashCooldownCounter < dashCooldown
+            || isWallJumping)
         {
             return;
         }
@@ -584,5 +648,37 @@ public class PlayerMovement : MonoBehaviour
             groundCheck.position +
             Vector3.down * glideGroundStopDistance
         );
+    }
+
+    private bool IsWalled()
+    {
+        bool isWalled = Physics2D.OverlapCircle(wallCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, groundLayer);
+    }
+
+    private void WallSlide()
+    {
+        if (IsWalled() && !IsGrounded() && IsMovingDown() && ((facingRight && horizontalInput > 0.01f) || (!facingRight && horizontalInput < -0.01f)))
+        {
+            timeBeforeSlippingCounter += Time.deltaTime;
+            isWallSliding = true;
+            if(isWallJumping)
+            {
+                isWallJumping = false;
+            }
+            if(timeBeforeSlippingCounter < timeBeforeSlipping)
+            {
+                playerRigidbody.linearVelocity = new Vector2(playerRigidbody.linearVelocityX, 0);
+            }
+            else
+            {
+                playerRigidbody.linearVelocity = new Vector2(playerRigidbody.linearVelocityX, Mathf.Clamp(playerRigidbody.linearVelocityY, -wallSlidingSpeed, float.MaxValue));
+            }
+        }
+        else
+        {
+            isWallSliding = false;
+            timeBeforeSlippingCounter = 0;
+        }
     }
 }
